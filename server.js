8cfcdaa5e9a7f829d5835c7c13c3d6c1eadd4c6f
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2026-01-14 16:58:15 +0000

    :boom: initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import Hapi from '@hapi/hapi';
+import Inert from '@hapi/inert';
+import H2o2 from '@hapi/h2o2';
+
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost'
+});
+
+await server.register([Inert, H2o2]);
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => h.file('index.html')
+});
+
+server.route({
+  method: 'GET',
+  path: '/wms',
+  handler: {
+    proxy: {
+      uri: 'https://map.bgs.ac.uk/arcgis/services/GeoIndex_Onshore/hydrogeology/MapServer/WmsServer{query}',
+      passThrough: true
+    }
+  }
+});
+
+await server.start();
+console.log('Server running on %s', server.info.uri);
